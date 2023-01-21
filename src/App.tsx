7908b3678eb9651591d501e5b2599c6ccdf016b0
Máté Bendegúz Kovács <20370225+kmb5@users.noreply.github.com>
2023-01-21 16:51:09 +0100

    remove not needed imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 import { TodoCard } from './components/TodoCard'
