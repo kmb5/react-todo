2008121b3aaa0deb3d87b25dee6d80c15f42efd2
Máté Bendegúz Kovács <20370225+kmb5@users.noreply.github.com>
2023-01-22 16:11:18 +0100

    add new todo to todolist, add unique key

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
-import * as React from 'react';
-
 import './App.css'
 
-import { TodoCard } from './components/TodoCard'
 import { TodoList } from './components/TodoList'
-import { NewTodo } from './components/NewTodo'
-
-import type { ITodo, ITodoList } from './components/TodoList'
 
 function App() {
-
-  const [todoText, setTodoText] = React.useState<string>('')
-  const [todos, setTodo] = React.useState<ITodoList>([])
-
-  const handleAddTodo = () => {
-    const td: ITodo = { todoText: todoText }
-    todos.push(td)
-    setTodo(todos)
-    setTodoText('')
-  }
-
-  const handleType = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTodoText(event.target.value)
-  }
 
   return (
     <div className="App">
 
-      <TodoList todoList={todos} />
-      <NewTodo value={todoText} onButtonPress={handleAddTodo} onType={handleType} />
+      <TodoList />
     </div>
   )
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,23 +1,50 @@
+import * as React from 'react';
 import { TodoCard } from './TodoCard'
 
 interface ITodo {
-    todoText: string
+    todoText: string,
+    objectID: number
 }
 
 interface ITodoList extends Array<ITodo> { }
 
-type TodoListProps = {
-    todoList: ITodoList
-}
 
-const TodoList = (props: TodoListProps) => {
+const TodoList = () => {
+
+    const [todoText, setTodoText] = React.useState<string>('');
+    const [todos, setTodo] = React.useState<ITodoList>([]);
+    const [oID, setOID] = React.useState<number>(0);
+
+    const handleAddTodo = () => {
+        const td: ITodo = { todoText: todoText, objectID: handleSetOID() }
+        todos.push(td)
+        setTodo(todos)
+        setTodoText('')
+        console.log(todos)
+    }
+
+    const handleType = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTodoText(event.target.value)
+    }
+
+    const handleSetOID = () => {
+        setOID(oID + 1)
+        return oID
+    }
+
     return (
-        <ul>
-            {props.todoList.map((item) => (
-                <TodoCard todoText={item.todoText} />
-            ))
-            }
-        </ul>
+        <div>
+            <ul>
+                {todos.map((item) => (
+                    <TodoCard key={item.objectID} todoText={item.todoText} />
+                ))
+                }
+            </ul>
+            <div>
+                <input id="todoText" type="text" placeholder="type something..." onChange={handleType} value={todoText} />
+                <button id="addTodo" onClick={handleAddTodo}>Add</button>
+            </div>
+        </div>
     )
 }
 
